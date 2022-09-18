40669ade32cd677d062d785536dcd478dbb02940
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-09-19 02:34:50 +0530

    changes in spotify clone

--- components/songsTable.tsx
+++ components/songsTable.tsx
@@ -6,13 +6,13 @@ import { useStoreActions } from 'easy-peasy';
 import { formatDate, formatTime } from '../lib/formatters';
 
 const SongTable = ({ songs }) => {
-  //   const playSongs = useStoreActions((store: any) => store.changeActiveSongs);
-  //   const setActiveSong = useStoreActions((store: any) => store.changeActiveSong);
+  const playSongs = useStoreActions((store: any) => store.changeActiveSongs);
+  const setActiveSong = useStoreActions((store: any) => store.changeActiveSong);
 
-  //   const handlePlay = (activeSong?) => {
-  //     setActiveSong(activeSong || songs[0]);
-  //     playSongs(songs);
-  //   };
+  const handlePlay = (activeSong?) => {
+    setActiveSong(activeSong || songs[0]);
+    playSongs(songs);
+  };
 
   return (
     <Box bg="transparent" color="white">
@@ -24,7 +24,7 @@ const SongTable = ({ songs }) => {
             colorScheme="green"
             size="lg"
             isRound
-            // onClick={() => handlePlay()}
+            onClick={() => handlePlay()}
           />
         </Box>
         <Table variant="unstyled">
@@ -49,7 +49,7 @@ const SongTable = ({ songs }) => {
                 }}
                 key={song.id}
                 cursor="pointer"
-                // onClick={() => handlePlay(song)}
+                onClick={() => handlePlay(song)}
               >
                 <Td>{i + 1}</Td>
                 <Td>{song.name}</Td>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { StoreProvider } from 'easy-peasy';
+import { store } from '../lib/store';
 import 'reset-css';
 import PlayerLayout from '../components/playerLayout';
 const theme = extendTheme({
@@ -32,13 +34,15 @@ const theme = extendTheme({
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      {Component.authPage ? (
-        <Component {...pageProps} />
-      ) : (
-        <PlayerLayout>
+      <StoreProvider store={store}>
+        {Component.authPage ? (
           <Component {...pageProps} />
-        </PlayerLayout>
-      )}
+        ) : (
+          <PlayerLayout>
+            <Component {...pageProps} />
+          </PlayerLayout>
+        )}
+      </StoreProvider>
     </ChakraProvider>
   );
 }
